bb21c023b36aae72d86d07d3d773bc44c8ee89ba
crimx <straybugs@gmail.com>
2019-09-06 11:27:32 +0800

    bump version

--- dist/core.js
+++ dist/core.js
@@ -53500,9 +53500,13 @@ window.browser.menus.create.callsFake(() => Promise.resolve())
 window.browser.menus.remove.callsFake(() => Promise.resolve())
 window.browser.menus.removeAll.callsFake(() => Promise.resolve())
 
-window.browser.contextMenus.create = window.browser.menus.create
-window.browser.contextMenus.remove = window.browser.menus.remove
-window.browser.contextMenus.removeAll = window.browser.menus.removeAll
+if (!window.browser.contextMenus) {
+  window.browser.contextMenus = {}
+}
+
+Object.keys(window.browser.menus).forEach(key => {
+  window.browser.contextMenus[key] = window.browser.menus[key]
+})
 
 
 /***/ }),
